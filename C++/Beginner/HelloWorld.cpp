55e800404b5fc197d9413111716a6da0e61eb7d4
ExDragine <surfaceluck@outlook.com>
2022-06-07 23:01:50 +0800

    Update to WSL

---
+++ C++/Beginner/HelloWorld.cpp
@@ -0,0 +1,8 @@
+// Hello world in C++
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
--- C/Beginner/00.Basic.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    char i = "Hello";
-    printf("%c", i);
-    system("pasue");
-    return 0;
-}
